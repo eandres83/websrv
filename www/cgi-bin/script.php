24c4bea484f6591d84631e26d982f9a5bf9d373f
nicolasque <quequecedo@gmail.com>
2025-09-17 21:29:17 +0200

    Enhance CGI management by adding environment variable setup and improving process handling; update PHP script output message for clarity.

--- www/cgi-bin/script.php
+++ www/cgi-bin/script.php
@@ -1,8 +1,8 @@
 <?php
 // script.php - Basic PHP template to test web server functionality
-
+echo "<h1>Hello world</h1>";
 // Display a simple message
-echo "¡El servidor web está funcionando correctamente!";
+echo "212El servidor web est funcionando correctamente!";
 
 // Display PHP information (optional, uncomment if needed)
 // phpinfo();
